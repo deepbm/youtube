ceb18d8c4306c2b57d30281e15d53e1ce0b59302
양희연 <deepbm27@gmail.com>
2023-06-15 16:09:48 +0900

    Add SearchHeader component

--- src/components/SearchHeader.jsx
+++ src/components/SearchHeader.jsx
@@ -1,5 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { BsSearch, BsYoutube } from 'react-icons/bs';
+import { Link, useNavigate, useParams } from 'react-router-dom';
 
-export default function SearchHeader() {
-  return <div>SearchHeader</div>;
+export default function Search() {
+  const navigate = useNavigate();
+  const { keyword } = useParams();
+  const [text, setText] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault();
+    navigate(`/videos/${text}`);
+  };
+  useEffect(() => {
+    setText(keyword || '');
+  }, [keyword]);
+
+  return (
+    <header className='flex p-4 border-b border-zinc-600 w-full text-xl'>
+      <Link className='flex items-center' to='/'>
+        <BsYoutube className='text-brand text-3xl' />
+        <span className='ml-2 font-bold text-2xl'>Youtube</span>
+      </Link>
+      <form className='flex justify-center w-full' onSubmit={handleSubmit}>
+        <input
+          className='p-2 w-7/12 outline-none bg-black text-gray-50'
+          type='text'
+          placeholder='Search...'
+          value={text}
+          onChange={e => setText(e.target.value)}
+        />
+        <button type='submit' className='px-4 bg-zinc-600'>
+          <BsSearch />
+        </button>
+      </form>
+    </header>
+  );
 }
