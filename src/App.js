84ed2ad8c653e35d93a79a19a851ee1cb02c462f
양희연 <deepbm27@gmail.com>
2023-06-15 14:58:54 +0900

    Add router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
+import { Outlet } from 'react-router-dom';
+import SearchHeader from './components/SearchHeader';
 
 function App() {
-  return <div></div>;
+  return (
+    <>
+      <SearchHeader />
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
