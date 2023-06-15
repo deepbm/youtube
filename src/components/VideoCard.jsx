fafd7d5a1dd4f8e854b8136bb968b469f5ea253a
양희연 <deepbm27@gmail.com>
2023-06-15 21:12:24 +0900

    Add move to VideoDetail

--- src/components/VideoCard.jsx
+++ src/components/VideoCard.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { formatAgo } from '../util/date';
+import { useNavigate } from 'react-router-dom';
 
 export default function VideoCard({ video }) {
+  const navigate = useNavigate();
   const {
     title,
     channelTitle,
@@ -10,8 +12,12 @@ export default function VideoCard({ video }) {
       medium: { url },
     },
   } = video.snippet;
+  const handleClick = () => {
+    navigate(`/videos/watch/${video.id}`, { state: { video } });
+  };
+
   return (
-    <li>
+    <li onClick={handleClick}>
       <img className='w-full' src={url} alt={title} />
       <div>
         <p className='my-2 font-semibold line-clamp-2'>{title}</p>
