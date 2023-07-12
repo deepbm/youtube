48e953602507fa4203b56667a7b61390f3daa59f
양희연 <deepbm27@gmail.com>
2023-07-12 21:59:02 +0900

    Update: save dark mode to localStorage

--- src/contexts/DarkMode.jsx
+++ src/contexts/DarkMode.jsx
@@ -3,10 +3,11 @@ import { createContext, useContext, useEffect, useState } from 'react';
 export const DarkModeContext = createContext();
 
 export function DarkModeProvider({ children }) {
-  const [mode, setMode] = useState(false);
+  const [mode, setMode] = useState(readModeFromLocalstorage);
   const toggleMode = () => setMode(prev => !prev);
 
   useEffect(() => {
+    localStorage.setItem('theme', mode ? 'dark' : 'light');
     if (mode) {
       document.body.classList.add('theme-dark');
     } else {
@@ -20,3 +21,8 @@ export function DarkModeProvider({ children }) {
 }
 
 export const useDarkMode = () => useContext(DarkModeContext);
+
+const readModeFromLocalstorage = () => {
+  const theme = localStorage.getItem('theme');
+  return theme === 'dark' ? true : false;
+};
