da1de107f6e4c7928211bccddff2e26b5434d161
양희연 <deepbm27@gmail.com>
2023-07-28 23:22:56 +0900

    Update NotFound.jsx

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function NotFound() {
-  return <div>NotFound</div>;
+  return (
+    <section className='flex justify-center items-center h-screen text-lg text-dark'>
+      <div>
+        <p>죄송합니다.</p>
+        <p className='mb-4'>요청하신 페이지를 찾을 수 없습니다.</p>
+        <Link to='/' replace>
+          <button className='px-4 rounded bg-zinc-600 text-white'>홈으로 가기</button>
+        </Link>
+      </div>
+    </section>
+  );
 }
