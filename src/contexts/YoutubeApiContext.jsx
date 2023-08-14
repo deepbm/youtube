c602db575c160735e1bb41bcb4ad3d7d527097ba
양희연 <deepbm27@gmail.com>
2023-08-14 14:04:35 +0900

    Update YoutubeApiContext.jsx

--- src/contexts/YoutubeApiContext.jsx
+++ src/contexts/YoutubeApiContext.jsx
@@ -1,10 +1,8 @@
 import { createContext, useContext } from 'react';
 import Youtube from '../api/youtube';
-import FakeYoutube from '../api/fakeYoutube';
 
 export const YoutubeApiContext = createContext();
 
-// const youtube = new FakeYoutube();
 const youtube = new Youtube();
 
 export function YoutubeApiProvider({ children }) {
